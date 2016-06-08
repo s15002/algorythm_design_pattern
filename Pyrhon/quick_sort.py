8533f4442172ce86b45cce07154b5b7232786c4b
Yuki Itoman <Yuki Itoman>
2016-06-08 11:10:09 +0900

    Add quick_sort

--- Pyrhon/quick_sort.py
+++ Pyrhon/quick_sort.py
@@ -1,5 +1,5 @@
 LIST_COUNT = 1000
-LOOP_COUNT = 10000
+LOOP_COUNT = 1000
 MAX_NUM = 10000
 
 def data_generate():
@@ -37,6 +37,29 @@ def quick_sort_2nd(data):
     return quick_sort_2nd([lt for lt in data[1:] if lt < data[0]]) + data[0:1] + quick_sort_2nd([ge for ge in data[1:] if ge >= data[0]])
 
 
+'''
+参考「並び替えのアルゴリズム」
+マージソート関数を利用したクイックソート
+'''
+def quick_sort_3rd(data):
+    def quick(list, left, right):
+        pl = left
+        pr = right
+        y = list[(pl + pr) / 2]
+        while True:
+            while list[pl] < y: pl += 1
+            while list[pr] > y: pr -= 1
+            if pl <= pr:
+                list[pl], list[pr] = list[pr], list[pl]
+                pl += 1
+                pr -= 1
+            if pl < pr:
+                break
+            if left < pr: quick(list, left, pr)
+            if pl < right: quick(list, pl, right)
+
+        quick(data, 0, len(data) - 1)
+
 # 経過時間
 if __name__ == '__main__':
     import time
@@ -67,3 +90,16 @@ if __name__ == '__main__':
     end = time.time()
     print()
     print('quick_sort_2nd:', (end - start))
+
+    #3rd
+    start = time.time()
+
+    for _ in range(LOOP_COUNT):
+        data = data_generate()
+        quick_sort_3rd(data)
+        print('.', end='')
+        sys.stdout.flush()
+
+    end = time.time()
+    print()
+    print('quick_sort_3rd:', (end - start))
